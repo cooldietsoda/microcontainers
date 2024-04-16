2a39d49e08c0ab315c3ef286d30919a567497749
thesodadev <thesodadev@protonmail.com>
2024-04-16 03:09:59 +0300

    Improve unordered set

--- include/microcontainers/set.h
+++ include/microcontainers/set.h
@@ -1,15 +1,79 @@
 #pragma once
 #include <cstdint>
+#include <cstring>
 
-namespace microcontainers {
+namespace microcontainers::set::unordered {
 
-template<typename T, typename SIZE_T, SIZE_T SIZE>
-class FixedSizeSerialAlignedSet {
+template<typename T, typename SIZE_T, SIZE_T CAPACITY>
+class FixedSizeInPlaceAllocated {
+public:
+  struct Iterator {
+    Iterator(FixedSizeInPlaceAllocated<T, SIZE_T, CAPACITY> &set, SIZE_T index) 
+      : m_set(set), m_currentIndex(index) {
+    }
+
+    T& operator*() const {
+      return m_set.m_elements[m_currentIndex];
+    }
+
+    T* operator->() {
+      return &m_set.m_elements[m_currentIndex];
+    }
+
+    Iterator& operator++() { 
+      if (m_currentIndex == CAPACITY-1) {
+        m_currentIndex = CAPACITY;
+        return *this;
+      }
+
+      for (SIZE_T i=m_currentIndex+1; i<CAPACITY; i++) {
+        if (m_set.m_usedElements[i]) {
+          m_currentIndex = i;
+          return *this;
+        }
+      }
+
+      return *this;
+    }
+
+    friend bool operator==(const Iterator& left, const Iterator& right) {
+      return left.m_currentIndex == right.m_currentIndex;
+    }
+
+    friend bool operator!=(const Iterator& left, const Iterator& right) {
+      return left.m_currentIndex != right.m_currentIndex;
+    }  
+
+  private:
+      FixedSizeInPlaceAllocated& m_set;
+      SIZE_T m_currentIndex;
+  };
+
+  Iterator begin() {
+    SIZE_T startIndex = 0;
+
+    for (SIZE_T i=0; i<CAPACITY; i++) {
+      if (m_usedElements[i]) {
+        startIndex = i;
+        break;
+      }
+    }
+
+    return Iterator(*this, startIndex);
+  }
+
+  Iterator end() {
+    return Iterator(*this, CAPACITY);
+  }
 
 public:
+  FixedSizeInPlaceAllocated() {
+    memset(m_usedElements, 0, sizeof(m_usedElements));
+  }
+
   bool insert(const T& element) {
-    for (SIZE_T i=0; i<SIZE; i++) {
-      if (!m_usedElements[i]) {
+    for (SIZE_T i=0; i<CAPACITY; i++) {
+      if (!m_usedElements[i] && m_elements[i] != element) {
         m_elements[i] = element;
         m_usedElements[i] = true;
         return true;
@@ -31,10 +95,37 @@ public:
   T& at(SIZE_T index) {
     return m_elements[index];
   }
+
+  SIZE_T size() {
+    SIZE_T n = 0;
+    for (SIZE_T i=0; i<CAPACITY; i++) {
+      if (m_usedElements[i]) {
+        n++;
+      }
+    }
+
+    return n;
+  }
+
+  SIZE_T capacity() {
+    return CAPACITY;
+  }
+
+  bool hasValue(const T& value) {
+    for (SIZE_T i=0; i<CAPACITY; i++) {
+      if (m_usedElements[i] && m_elements[i] == value) {
+        return true;
+      }
+    }
+
+    return false;
+  }
   
 private:
-  T m_elements[SIZE];
-  bool m_usedElements[SIZE];
+  T m_elements[CAPACITY];
+  bool m_usedElements[CAPACITY];
+
+  friend struct Iterator;
 };
 
 }
--- test/set_test.cpp
+++ test/set_test.cpp
@@ -1,7 +1,84 @@
 #include <microcontainers/set.h>
+#include <DamnTest/damntest.h>
+#include <cstdio>
+#include <cstdlib>
 
-int main() {
-  microcontainers::FixedSizeSerialAlignedSet<char, int, 10> set;
-  set.insert('a');
-  return 0;
+void insertOverflow() {
+  using namespace microcontainers::set::unordered;
+  const int capacity = 4;
+  FixedSizeInPlaceAllocated<char, int, capacity> set;
+  DAMN_ASSERT(set.insert('a'));
+  DAMN_ASSERT(set.insert('b'));
+  DAMN_ASSERT(set.insert('c'));
+  DAMN_ASSERT(set.insert('d'));
+
+  DAMN_ASSERT(!set.insert('e'));
+}
+
+void valuePresent() {
+  using namespace microcontainers::set::unordered;
+  const int capacity = 4;
+  FixedSizeInPlaceAllocated<char, int, capacity> set;
+  DAMN_ASSERT(set.insert('a'));
+  DAMN_ASSERT(set.insert('b'));
+  DAMN_ASSERT(set.insert('c'));
+  DAMN_ASSERT(set.insert('d'));
+
+  DAMN_ASSERT(set.hasValue('b'));
+  DAMN_ASSERT(set.hasValue('c'));
+}
+
+void iteratorIncrement() {
+  using namespace microcontainers::set::unordered;
+  const int capacity = 4;
+  FixedSizeInPlaceAllocated<char, int, capacity> set;
+  DAMN_ASSERT(set.insert('a'));
+  DAMN_ASSERT(set.insert('b'));
+  DAMN_ASSERT(set.insert('c'));
+  DAMN_ASSERT(set.insert('d'));
+
+  auto itBegin = set.begin();
+  auto itEnd = set.end();
+
+  DAMN_ASSERT(*itBegin == 'a'); ++itBegin;
+  DAMN_ASSERT(*itBegin == 'b'); ++itBegin;
+  DAMN_ASSERT(*itBegin == 'c'); ++itBegin;
+  DAMN_ASSERT(*itBegin == 'd'); ++itBegin;
+  DAMN_ASSERT(itBegin == itEnd);
+}
+
+namespace DamnTest {
+const char* getTestSuiteName() {
+  return "FixedSizeSerialAlignedSet";
+}
+
+void puts(const char* msg) {
+  printf("%s", msg);
+}
+
+void exit(int status) {
+  ::exit(status);
+}
+
+void putTestCases() {
+  addTestCase("insertOverflow", insertOverflow);
+  addTestCase("valuePresent", valuePresent);
+  addTestCase("iteratorIncrement", iteratorIncrement);
+}
+
+bool preTestCase() {
+  return true;
+}
+
+bool postTestCase() {
+  return true;
+}
+
+bool preTestSuite() {
+  return true;
+}
+
+bool postTestSuite() {
+  return true;
+}
 }
